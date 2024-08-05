3fe5a47afc454b6ea3bba6e4361f096acaf95545
Wilver Emilio Xiá Ixcot <wilb123@umes.edu.gt>
2024-08-05 09:01:53 -0600

    Nueva estructura
    Se cambio la estructura del proyecto, para poder utilizarlo con venv

--- BackEnd/app/conexion.py
+++ BackEnd/app/conexion.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://root:EB163Q7E@localhost:3306/abarroteria"
+# A continuación se define la URL de conexión a la base de datos, seleccionar cual de las siguientes líneas se va a utilizar
+
+# DATABASE_URL = "mysql+mysqlconnector://root:EB163Q7E@localhost:3306/abarroteria" #Conectarse a la base de datos mediente contraseña
+# DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/abarroteria" #Conectarse a la base de datos sin contraseña
+DATABASE_URL = "mysql+mysqlconnector://root:123456789@localhost:3306/abarroteria" 
 
 engine = create_engine(DATABASE_URL)
 
